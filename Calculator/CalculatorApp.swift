7192f2961329c70951ca364232058c922e7c0dff
Boris Kravchenko <allmycalifornia@gmail.com>
2024-11-22 10:49:09 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Борис Кравченко on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
